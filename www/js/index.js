c4d955b72151d7c17f9d3171e47e780f5ef13e34
EthanBristow <brie1_15@uni.worc.ac.uk>
2016-11-15 11:44:51 +0000

    beep plugin added

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,14 @@ $(document).on("pagecreate", "#pageone", function() {
   $("#OutputButton").on("click", function() {
     var R = random();
     $(this).text("Random value: " + R);
+      
+      if (R == true) {
+          navigator.notification.beep(1);
+      }
+      else if (R == false) {
+          navigator.notification.beep(2);
+      }
+               
   });            
 });            
 
