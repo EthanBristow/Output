161933faa9cba6423ee480bb6fc6426d0538d520
EthanBristow <brie1_15@uni.worc.ac.uk>
2016-11-15 10:05:26 +0000

    Files added

---
+++ www/js/index.js
@@ -0,0 +1,17 @@
+$(document).on("pagecreate", "#pageone", function() {
+  $('#OutputButton').on("click", function() {
+    random();
+  });            
+});            
+
+
+function random() {
+	return !Math.round(Math.random());
+    $("#text")
+}
+
+
+/* function storeValue(key, value) {
+    window.localStorage.setItem(key, value);
+} */
+
